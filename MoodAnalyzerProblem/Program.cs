bd84f5adae573b2e55b1ae488961bd7d22e9627c
Lenovo <somusp75@gmail.com>
2022-01-26 18:39:31 +0530

    mood message in constructor TC 1.1 and 1.2

--- MoodAnalyzerProblem/Program.cs
+++ MoodAnalyzerProblem/Program.cs
@@ -6,7 +6,11 @@ namespace MoodAnalyzerProblem
     {
         static void Main(string[] args)
         {
+            //Refactror a To intialize parameter
             Console.WriteLine("--------------Welcome To Mood Analyzer Problem------------");
+            MoodAnalyze moodAnalyze = new MoodAnalyze("I am in Happy Mood");
+            string mood = moodAnalyze.AnalyzeMood();
+
         }
     }
 }
